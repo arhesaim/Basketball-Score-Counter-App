d25268f9c0fa997d2a00a197bfbc740b90100d9f
arhesaim <katsiaryna.two@gmail.com>
2023-03-17 13:39:27 +0200

    livedata implemented

--- app/src/main/java/com/example/basketballscorecounter/MainActivity.java
+++ app/src/main/java/com/example/basketballscorecounter/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.basketballscorecounter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.view.View;
@@ -17,7 +19,29 @@ public class MainActivity extends AppCompatActivity {
     private View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-
+            switch (v.getId()){
+                case R.id.score3Team1btn:
+                    addTeam1(R.id.score3Team1btn);
+                    break;
+                case R.id.score2Team1btn:
+                    addTeam1(R.id.score2Team1btn);
+                    break;
+                case R.id.score1Team1btn:
+                    addTeam1(R.id.score1Team1btn);
+                    break;
+                case R.id.score3Team2btn:
+                    addTeam2(R.id.score3Team2btn);
+                    break;
+                case R.id.score2Team2btn:
+                    addTeam2(R.id.score2Team2btn);
+                    break;
+                case R.id.score1Team2btn:
+                    addTeam2(R.id.score1Team2btn);
+                    break;
+                case R.id.resetbtn:
+                    resetScore();
+                    break;
+            }
         }
     };
 
@@ -25,6 +49,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
+
+        mainViewModel.scoreTeam1.observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                scoreTeam1.setText(String.valueOf(mainViewModel.scoreTeam1.getValue()));
+            }
+        });
+        mainViewModel.scoreTeam2.observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                scoreTeam2.setText(String.valueOf(mainViewModel.scoreTeam2.getValue()));
+            }
+        });
 
         score3Team1btn = findViewById(R.id.score3Team1btn);
         score3Team2btn = findViewById(R.id.score3Team2btn);
@@ -45,6 +84,29 @@ public class MainActivity extends AppCompatActivity {
         score1Team2btn.setOnClickListener(onClickListener);
         resetbtn.setOnClickListener(onClickListener);
 
+    }
 
+    public void addTeam1(int id){
+        if (id == R.id.score3Team1btn){
+            mainViewModel.add3Team1Score();
+        } else if (id == R.id.score2Team1btn){
+            mainViewModel.add2Team1Score();
+        } else if (id == R.id.score1Team1btn){
+            mainViewModel.add1Team1Score();
+        }
+    }
+
+    public void addTeam2(int id){
+        if (id == R.id.score3Team2btn){
+            mainViewModel.add3Team2Score();
+        } else if (id == R.id.score2Team2btn){
+            mainViewModel.add2Team2Score();
+        } else if (id == R.id.score1Team2btn){
+            mainViewModel.add1Team2Score();
+        }
+    }
+
+    public void resetScore(){
+        mainViewModel.resetScore();
     }
 }
--- app/src/main/java/com/example/basketballscorecounter/MainViewModel.java
+++ app/src/main/java/com/example/basketballscorecounter/MainViewModel.java
@@ -1,7 +1,42 @@
 package com.example.basketballscorecounter;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MainViewModel extends ViewModel {
-    public int scoreTeam1, scoreTeam2;
+    public MutableLiveData<Integer> scoreTeam1 = new MutableLiveData<>();
+    public MutableLiveData<Integer> scoreTeam2 = new MutableLiveData<>();
+
+    public MainViewModel() {
+        scoreTeam1.setValue(0);
+        scoreTeam2.setValue(0);
+    }
+
+    public void add3Team1Score(){
+        scoreTeam1.setValue(scoreTeam1.getValue() + 3);
+    }
+
+    public void add2Team1Score(){
+        scoreTeam1.setValue(scoreTeam1.getValue() + 2);
+    }
+
+    public void add1Team1Score(){
+        scoreTeam1.setValue(scoreTeam1.getValue() + 1);
+    }
+
+    public void add3Team2Score(){
+        scoreTeam2.setValue(scoreTeam2.getValue() + 3);
+    }
+
+    public void add2Team2Score(){
+        scoreTeam2.setValue(scoreTeam2.getValue() + 2);
+    }
+
+    public void add1Team2Score(){
+        scoreTeam2.setValue(scoreTeam2.getValue() + 1);
+    }
+    public void resetScore(){
+        scoreTeam1.setValue(0);
+        scoreTeam2.setValue(0);
+    }
 }
